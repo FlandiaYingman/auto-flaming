6f5e53c6654109119c5c654665fc8e6310713e03
Flandia Yingman <Flandia_YingM@hotmail.com>
2021-10-11 23:25:37 +0800

    feat: new version 2.0, many improvements

--- README.md
+++ README.md
@@ -2,30 +2,37 @@
 
 帮助广大健忘症患者自动续火。
 
-## Using ##
+## 使用 ##
 
 首先，安装包：
+
 ```
 PyAutoGUI==0.9.52
 pyperclip==1.8.1
 pywin32==300
-schedule==0.6.0
 ```
 
-然后，运行`flame_now.py`或`flame_sch.py`，取决于你想在现在续火一次还是计划每天的续火。
+然后，自定义参数：
 
-## 自定义参数 ##
 `flaming.py`下：
 ```python
-FLAMING_COUNT = 15 # 续火人数
-FLAMING_MESSAGES = [ # 续火信息。从列表中随机抽取，*开头的为图片
-    "*小丑竟是我自己",
-    "*好吧，我是小丑"
+# 续火联系人数量
+FLAMING_COUNT = 9
+# 续火消息，将在列表中随机抽取
+FLAMING_MESSAGES = [
+    "早上好",
+    "晚上好",
 ]
-WHEEL_CLICKS_PER_PERSON = -45 # 鼠标滑轮滚过一个联系人的click数量
+
+# 滚轮滑过一名联系人所需要的 click 数，通常不需要更改
+WHEEL_CLICKS_PER_PERSON = -45
+
+# 64位系统使用
+QQ_HOME = Path(r"C:\Program Files (x86)\Tencent\QQ\Bin")
+# # 32位系统使用
+# QQ_HOME = Path(r"C:\Program Files\Tencent\QQ\Bin")
 ```
 
-`flame_sch.py`下：
-```python
-FLAMING_TIME = '09:00' # 续火时间。格式为"HH:MM"
-```
+然后，保证 QQ 的登录模式非自动登录，且已保存密码，所有续火联系人都是置顶。
+
+运行`flaming.py`，开始续火。
--- flaming.py
+++ flaming.py
@@ -1,70 +1,99 @@
-import os
 import random
+import subprocess
 import time
+from dataclasses import dataclass
+from pathlib import Path
 
+import psutil
 import pyautogui as auto
+import pyperclip
 import pywintypes
 import win32gui
 
-import clip
-
-FLAMING_COUNT = 15
+# 续火联系人数量
+FLAMING_COUNT = 9
+# 续火消息，将在列表中随机抽取
 FLAMING_MESSAGES = [
-    "*小丑竟是我自己",
-    "*好吧，我是小丑"
+    "早上好",
+    "晚上好",
 ]
+
+# 滚轮滑过一名联系人所需要的 click 数，通常不需要更改
 WHEEL_CLICKS_PER_PERSON = -45
 
+# 64位系统使用
+QQ_HOME = Path(r"C:\Program Files (x86)\Tencent\QQ\Bin")
+# # 32位系统使用
+# QQ_HOME = Path(r"C:\Program Files\Tencent\QQ\Bin")
 
-def re_login():
-    os.system('taskkill /f /im QQ.exe')
-    time.sleep(1.0)
 
-    os.system('"C:\\Program Files (x86)\\Tencent\\QQ\\Bin\\QQScLauncher.exe"')
-    time.sleep(1.0)
+@dataclass
+class WindowRect:
+    x: int
+    y: int
+    width: int
+    height: int
 
-    while True:
-        try:
-            qq_rect()
-            fail = False
-        except pywintypes.error:
-            fail = True
-        time.sleep(1.0)
-        if not fail:
-            break
 
-    auto.click(960, 650)
-    while not qq_rect()[3] > 540:
-        time.sleep(1.0)
+def launch_qq():
+    subprocess.Popen("C:\\Program Files (x86)\\Tencent\\QQ\\Bin\\QQ.exe")
+
+
+def stop_qq():
+    qq_procs = [proc for proc in psutil.process_iter() if proc.name() == 'QQ.exe']
+    for qq_proc in qq_procs:
+        qq_proc.kill()
+
+
+def login():
+    wait_for_login_window()
+    rect = qq_rect()
+    auto.click(rect.x + (rect.width / 2), rect.y + 345)
+    wait_for_main_window()
     time.sleep(3.0)
 
 
-def flame():
-    x, y, w, h = qq_rect()
-    auto.PAUSE = 1.0
+def wait_for_main_window():
+    max_login_window_height = 470
+    min_main_window_height = 600
+    while not qq_rect().height > (max_login_window_height + min_main_window_height) / 2:
+        time.sleep(1.0)
 
-    # Move cursor to the top of the QQ window
-    auto.moveTo(x + 25, 0)
-    auto.moveRel(0, 25)
+
+def wait_for_login_window():
+    while True:
+        try:
+            qq_rect()
+            time.sleep(1.0)
+            break
+        except pywintypes.error:
+            pass
+
+
+def flame():
+    auto.PAUSE = 1.0
+    rect = qq_rect()
 
     # Drag the QQ window out of the border of screen
-    auto.click()
-    auto.dragRel(0, 25, duration=1.0)
-    auto.click()
-    auto.dragRel(0, 25, duration=1.0)
-
-    x, y, w, h = qq_rect()
+    while rect.y < 0:
+        auto.moveTo(rect.x + 15, rect.y)
+        rect = qq_rect()
+        auto.moveTo(rect.x + 15, rect.y + 15)
+        auto.doubleClick()
+        auto.dragRel(0, 30, duration=0.25)
+        rect = qq_rect()
 
     # Move cursor to the first person's avatar
-    auto.moveTo(x + 40, y + 220)
+    auto.moveTo(rect.x + 40, rect.y + 220)
 
     # Scroll to the last person's avatar
     auto.scroll(10000)  # Always start at the first person
     for i in range(FLAMING_COUNT - 1):
         auto.scroll(WHEEL_CLICKS_PER_PERSON)
 
+    # Flame
     for i in range(FLAMING_COUNT):
-        clip.copy(random.choice(FLAMING_MESSAGES))
+        pyperclip.copy(random.choice(FLAMING_MESSAGES))
         auto.doubleClick()
         time.sleep(1.0)
         auto.hotkey('ctrl', 'v')
@@ -72,24 +101,22 @@ def flame():
         time.sleep(2.0)
     auto.hotkey('alt', 'f4')
 
-    # Reset the person list
-    auto.scroll(10000)
 
-    # Move cursor to the top of the QQ window
-    auto.moveTo(x + 25, y + 20)
-
-    # Drag the QQ window to the border of screen
-    auto.dragRel(0, -100, duration=0.5)
-
-    # Move the cursor to the center of screen
-    auto.moveTo(1920 / 2, 1080 / 2)
+def qq_hwnd():
+    hwnd = win32gui.FindWindow(None, 'QQ')
+    return hwnd
 
 
 def qq_rect():
-    hwnd = win32gui.FindWindow(None, 'QQ')
+    hwnd = qq_hwnd()
     rect = win32gui.GetWindowRect(hwnd)
-    x = rect[0]
-    y = rect[1]
-    w = rect[2] - x
-    h = rect[3] - y
-    return x, y, w, h
+    return WindowRect(rect[0], rect[1], rect[2] - rect[0], rect[3] - rect[1])
+
+
+if __name__ == '__main__':
+    stop_qq()
+
+    launch_qq()
+    login()
+    flame()
+    stop_qq()
